c8cf4fdccb42b16d98d043666ca1ca76aea28d4a
Priyanshu Rawat <rawatpriyanshu653@gmail.com>
2024-11-26 01:22:12 +0530

    update

---
+++ websiteUsingReactRouter/src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './componets/Home/Home.jsx'
+import About from './componets/About/About.jsx'
+import Contact from './componets/Contact/Contact.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "About",
+        element: <About />
+      },
+      {
+        path: "Contact",
+        element: <Contact />
+      }
+    ]
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
